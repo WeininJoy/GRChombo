d4645d9930bb47cfca1dff614ba8cbf81d06f84e
KAClough <katyclough@yahoo.com>
2021-06-21 11:25:12 +0100

    Oscillaton egs

--- Examples/Oscillaton/plotenergyLL.py
+++ Examples/Oscillaton/plotenergyLL.py
@@ -14,13 +14,13 @@ fig = plt.figure()
 # volume integral dataset
 data1 = np.loadtxt("VolumeIntegrals.dat")
 timedata = data1[:,0]
-dM = symmetry*data1[:,2] - symmetry*data1[0,2]
-Source = symmetry*data1[:,4]
+dM = symmetry*data1[:,1] - symmetry*data1[0,1]
+Source = symmetry*data1[:,3]
 
 # flux dataset integration
 data1 = np.loadtxt("SurfaceIntegrals.dat")
 timedata = data1[:,0]
-NetEoFlux = data1[:,2]
+NetEoFlux = data1[:,1]
 f_flux = interpolate.interp1d(timedata, NetEoFlux, kind = 'cubic')
 f_source = interpolate.interp1d(timedata, Source, kind = 'cubic')
 finetimedata = np.linspace(timedata[0], timedata[np.size(timedata)-1], 10000)
@@ -35,11 +35,10 @@ plt.figure(figsize=(8,6))
 plt.plot(timedata, dM, '-', lw = 2.0, label=r'$\int (Q-Q_0) dV$')
 plt.plot(finetimedata, Sourcedt, '--', lw = 1.5, label=r'$\int \int \mathcal{S} dV dt$')
 plt.plot(finetimedata, FEodt, ':', lw = 2.0, label=r'$\int \int F dS dt$')
-
 # make the plot look nice
 plt.rc('axes', titlesize=16)
 plt.xlabel("simulation time")
-plt.ylabel("energy")
+plt.ylabel("energy (LL)")
 plt.xlim(0, 82)
 plt.ylim(-0.022, 0.022)
 plt.rc('legend', fontsize=14)
@@ -47,6 +46,6 @@ plt.legend(loc=0)
 plt.grid()
 
 # save as png image
-filename = "EvsT_KV.png"
+filename = "EvsT_LL.png"
 plt.tight_layout()
 plt.savefig(filename)
